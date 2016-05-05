d1402b86b1ba73a96cd90fe2dcf03436d84db759
Mave <info@wesleybuurke.nl>
2016-05-05 02:56:56 +0200

    Milestone no > 100, more margin for name

--- views/milestone/_info.php
+++ views/milestone/_info.php
@@ -1,10 +1,14 @@
 <?php
 global $milestoneCount;
-foreach($milestones as $milestone) { ?>
+foreach($milestones as $milestone) {
+	$milestoneCount++; ?>
 
 							<tr>
 								<td class="text-left milestone-item">
-									<span class="milestone-no"><?= ++$milestoneCount; ?></span>
+									<span class="milestone-no<?= $milestoneCount >= 100 ? ' milestone-100' : ''; ?>">
+										<?= $milestoneCount; ?>
+
+									</span>
 									<span class="milestone-name"><?= $milestone->name; ?></span>
 								</td>
 								<td class="text-right milestone-item milestone-time"><small><?= $milestone->time; ?></small></td>
